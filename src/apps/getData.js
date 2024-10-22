56520826776bf50af8097d4bc10a074de0f0f407
Fred Mark Baldeviso <fredmark.baldeviso@gmail.com>
2024-10-23 00:28:12 +0800

    Use new api key

--- src/apps/getData.js
+++ src/apps/getData.js
@@ -1,6 +1,6 @@
 const baseURL = 'https://api.weatherapi.com/v1';
 const xkrcd = [];
-'8>8:5f9g69659>f;=9h6<9:8:787965'.split('').forEach(char => xkrcd.push(String.fromCharCode(char.charCodeAt(0) - 5)));
+'i5if5:8>j<;j9fkig7<6;7;:6797765'.split('').forEach(char => xkrcd.push(String.fromCharCode(char.charCodeAt(0) - 5)));
 
 const getDataFromAPI = async function (location) {
     const forecastRequestURL = `${baseURL}/forecast.json?key=${xkrcd.join('')}&q=${location}&days=3`;
